4da8e6c11117f5225f672e08ac36c259656c6740
IcaroJustino <icarojustinom@gmail.com>
2024-09-09 10:52:12 -0300

    responsividade do login

--- frontend/src/components/forms/loginForm.tsx
+++ frontend/src/components/forms/loginForm.tsx
@@ -19,13 +19,13 @@ export default function LoginForm() {
         const password = (event.target as HTMLFormElement).password.value;
 
 
-        if(!email || !password) {
+        if (!email || !password) {
             setError(true);
             setErrormsg('Preencha todos os campos');
             return;
         }
         const response = await loginService(email, password);
-        if(response.status === 400) {
+        if (response.status === 400) {
             setError(true);
             setErrormsg('Email ou senha inválidos');
             return;
@@ -34,34 +34,34 @@ export default function LoginForm() {
         setTimeout(() => {
             setLoading(false);
             setError(false);
-            navigate("/dashboard", { replace: true })
+            navigate("/", { replace: true })
         }, 3000);
         console.log(email, password);
     }
 
 
 
-    return(
-        <form onSubmit={handlesubmit} className="min-w-[28rem] min-h-[24rem] h-fit w-fit flex flex-col p-3 m-auto ">
+    return (
+        <form onSubmit={handlesubmit} className="lg:min-w-[28rem] lg:min-h-[24rem] lg:h-fit h-full w-fit flex flex-col p-3 m-auto ">
             <h1 className="text-3xl font-semi-bold text-primary">Entre na sua conta</h1>
             <span className="mt-4 mb-10 text-subtext font-normal text-medium">Boas-vindas! Por favor, insira suas credenciais para acessar os sistemas da Comigo.</span>
             {
                 error && error ? <div className="bg-red-200 p-2 text-red-500 text-sm mb-4">{errormsg}</div> : ''
             }
-            <input type="text" placeholder="Email" name="email" id="email" className="p-2 border-input border-2 text-input mb-6 rounded-md" required/>
+            <input type="text" placeholder="Email" name="email" id="email" className="p-2 border-input border-2 text-input mb-6 rounded-md" required />
             <input type="password" placeholder="Senha" name="password" id="password" className="p-2 border-input border-2 text-input rounded-md" required />
             <div className="flex flex-row justify-between mt-6 mb-8">
                 <div>
-                    <input type="checkbox" name="remember-me" id="remember-me" className="mr-2 rounded-sm"/>
+                    <input type="checkbox" name="remember-me" id="remember-me" className="mr-2 rounded-sm" />
                     <label htmlFor="remember-me" className="font-normal text-sm text-checkbox">Mantenha-me conectado.</label>
                 </div>
                 <a href="#" className="text-hyperlink">Esqueci minha senha</a>
             </div>
             {
-                loading  && loading ? <div className="flex justify-center items-center h-12 w-full rounded-md bg-primary "><Loader/></div> : 
-                <button type="submit" className="  text-white tex-center text-medium font-semi-bold bg-primary h-12 w-full rounded-md ">Entrar</button>
+                loading && loading ? <div className="flex justify-center items-center h-12 w-full rounded-md bg-primary "><Loader /></div> :
+                    <button type="submit" className="  text-white tex-center text-medium font-semi-bold bg-primary h-12 w-full rounded-md ">Entrar</button>
             }
-          
+
         </form>
     )
 };
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,16 +1,24 @@
 import LoginForm from "../components/forms/loginForm"
 import loginimage from '../assets/static/loginBackground.svg'
+import { useEffect } from "react";
 
 export default function Login() {
-    return(
-        <div className="flex lg:flex-row flex-col-reverse w-screen h-screen max-w-1080 justify-center mx-auto">
-            <div className="lg:w-1/2 w-full lg:h-ful h-full flex flex-col justify-center bg-primary-light">
-                <LoginForm />
+    useEffect(() => {
+        document.title = "Acesse a sua conta";
+    }, []);
+
+
+    return (
+        <>
+            <div className="flex lg:flex-row flex-col-reverse w-screen h-screen max-w-1080 justify-center mx-auto">
+                <div className="lg:w-1/2 w-full lg:h-full h-full flex flex-col justify-center bg-primary-light">
+                    <LoginForm />
+                </div>
+                <div className="lg:w-1/2 w-full lg:h-full h-fit flex  justify-center bg-secondary ">
+                    <img src={loginimage} alt="login" className="h-full w-[30rem] lg:flex hidden" />
+                    <span className="md:flex sm:flex lg:hidden text-white font-semi-bold text-lg ml-2 justify-start my-2 py-2 w-full ">Login</span>
+                </div>
             </div>
-            <div className="lg:w-1/2 w-full lg:h-full h-[274px] flex  justify-center bg-secondary ">
-                <img src={loginimage} alt="login" className="h-full w-[30rem] lg:flex hidden"/>
-                <span className="md:flex sm:flex lg:hidden text-white font-semi-bold text-lg ml-2 justify-start my-2">Login</span>
-            </div>
-        </div>
+        </>
     )
 };
